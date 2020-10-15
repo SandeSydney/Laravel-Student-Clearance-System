d29f60cb14c545ada8660aec45dec4d87f245385
SandeSydney <sandesydney01@gmail.com>
2020-10-15 10:28:35 +0300

    added Maseno University logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+            <img src="{{asset('images/MU-Logo.png')}}" alt="Maseno University Logo" width="50">
+                <a class="navbar-brand ml-2" href="{{ url('/') }}">
+                    {{ config('app.name', 'MSUSCI-SCS') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
