0ab1977cbd7f81a4abe78ef5b064bd2221beb6cc
SandeSydney <sandesydney01@gmail.com>
2020-10-19 10:52:22 +0300

    added user profile and notifications sections

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,4 +9,12 @@ class UserController extends Controller
     public function clearanceForm(){
         return(view('clearance-form'));
     }
+
+    public function userProfile(){
+        return(view('user-profile'));
+    }
+
+    public function notifications(){
+        return(view('notifications'));
+    }
 }
