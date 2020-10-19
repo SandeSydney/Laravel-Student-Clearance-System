b6f925788884134e63066482510feb34fd1d71dd
SandeSydney <sandesydney01@gmail.com>
2020-10-19 10:53:59 +0300

    added routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/clearance-form', 'App\Http\Controllers\UserController@clearanceForm');
+
+Route::get('/user-profile', 'App\Http\Controllers\UserController@userProfile');
+
+Route::get('/notifications', 'App\Http\Controllers\UserController@notifications');
 
 Route::get('/admin', 'App\Http\Controllers\AdminController@index');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
