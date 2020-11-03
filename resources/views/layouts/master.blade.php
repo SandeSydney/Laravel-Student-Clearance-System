03693e328de991665979b57d4cbc5677ccf3b9bf
SandeSydney <sandesydney01@gmail.com>
2020-11-03 05:25:05 +0300

    Made container fluid, Removed sample dashboard-ui css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -32,152 +32,150 @@
   <!-- CSS Files -->
   <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
   <link href="../assets/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="../assets/demo/demo.css" rel="stylesheet" />
 </head>
 
-<body class="">
-  <div class="wrapper ">
+<body class="container-fluid">
+    <div class="wrapper ">
 
-    <!-- sidebar-->
-    <div class="sidebar" data-color="blue"><!--Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"-->
-      <div class="logo">
-        <a href="#" class="simple-text logo-mini">
-        <img src="{{asset('images/MU-Logo.png')}}" alt="">
-        </a>
-        <a href="#" class="simple-text logo-normal">
-          MSUSCI: SCS
-        </a>
-      </div>
-      <div class="sidebar-wrapper" id="sidebar-wrapper">
-        <ul class="nav">
-          <li>
-            <a href="/admin">
-              <i class="now-ui-icons design_app"></i>
-              <p>Dashboard</p>
+        <!-- sidebar-->
+        <div class="sidebar" data-color="blue"><!--Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"-->
+        <div class="logo">
+            <a href="#" class="simple-text logo-mini">
+            <img src="{{asset('images/MU-Logo.png')}}" alt="">
             </a>
-          </li>
+            <a href="#" class="simple-text logo-normal">
+            MSUSCI: SCS
+            </a>
+        </div>
+        <div class="sidebar-wrapper" id="sidebar-wrapper">
+            <ul class="nav">
+            <li>
+                <a href="/admin">
+                <i class="now-ui-icons design_app"></i>
+                <p>Dashboard</p>
+                </a>
+            </li>
 
-          <li>
-            <a href="/admin/notifications">
-              <i class="now-ui-icons ui-1_bell-53"></i>
-              <p>Notifications</p>
-            </a>
-          </li>
-          <li>
-            <a href="/admin/user">
-              <i class="now-ui-icons users_single-02"></i>
-              <p>User Profile</p>
-            </a>
-          </li>
-          <li class="">
-            <a href="/admin/tables">
-              <i class="now-ui-icons design_bullet-list-67"></i>
-              <p>Table List</p>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="main-panel" id="main-panel">
+            <li>
+                <a href="/admin/notifications">
+                <i class="now-ui-icons ui-1_bell-53"></i>
+                <p>Notifications</p>
+                </a>
+            </li>
+            <li>
+                <a href="/admin/user">
+                <i class="now-ui-icons users_single-02"></i>
+                <p>User Profile</p>
+                </a>
+            </li>
+            <li class="">
+                <a href="/admin/tables">
+                <i class="now-ui-icons design_bullet-list-67"></i>
+                <p>Table List</p>
+                </a>
+            </li>
+            </ul>
+        </div>
+        </div>
+        <div class="main-panel" id="main-panel">
 
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
-        <div class="container-fluid">
-          <div class="navbar-wrapper">
-            <div class="navbar-toggle">
-              <button type="button" class="navbar-toggler">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </button>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
+            <div class="container-fluid">
+            <div class="navbar-wrapper">
+                <div class="navbar-toggle">
+                <button type="button" class="navbar-toggler">
+                    <span class="navbar-toggler-bar bar1"></span>
+                    <span class="navbar-toggler-bar bar2"></span>
+                    <span class="navbar-toggler-bar bar3"></span>
+                </button>
+                </div>
+                <a class="navbar-brand" href="">@yield('navbar-brand')</a>
             </div>
-            <a class="navbar-brand" href="">@yield('navbar-brand')</a>
-          </div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navigation">
-            <form>
-              <div class="input-group no-border">
-                <input type="text" value="" class="form-control" placeholder="Search...">
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <i class="now-ui-icons ui-1_zoom-bold"></i>
-                  </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-bar navbar-kebab"></span>
+                <span class="navbar-toggler-bar navbar-kebab"></span>
+                <span class="navbar-toggler-bar navbar-kebab"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navigation">
+                <form>
+                <div class="input-group no-border">
+                    <input type="text" value="" class="form-control" placeholder="Search...">
+                    <div class="input-group-append">
+                    <div class="input-group-text">
+                        <i class="now-ui-icons ui-1_zoom-bold"></i>
+                    </div>
+                    </div>
                 </div>
-              </div>
-            </form>
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#pablo">
-                  <i class="now-ui-icons media-2_sound-wave"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Stats</span>
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="now-ui-icons location_world"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Some Actions</span>
-                  </p>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">Action</a>
-                  <a class="dropdown-item" href="#">Another action</a>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#pablo">
-                  <i class="now-ui-icons users_single-02"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Account</span>
-                  </p>
-                </a>
-              </li>
-            </ul>
-          </div>
+                </form>
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#pablo">
+                    <i class="now-ui-icons media-2_sound-wave"></i>
+                    <p>
+                        <span class="d-lg-none d-md-block">Stats</span>
+                    </p>
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="now-ui-icons location_world"></i>
+                    <p>
+                        <span class="d-lg-none d-md-block">Some Actions</span>
+                    </p>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#pablo">
+                    <i class="now-ui-icons users_single-02"></i>
+                    <p>
+                        <span class="d-lg-none d-md-block">Account</span>
+                    </p>
+                    </a>
+                </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+
+        <!-- The content section-->
+        <div class="panel-header panel-header-sm">
         </div>
-      </nav>
-      <!-- End Navbar -->
 
-      <!-- The content section-->
-      <div class="panel-header panel-header-sm">
-      </div>
+        @yield('content')
 
-      @yield('content')
-
-      <!-- Footer Section-->
-      <footer class="footer">
-        <div class="container-fluid">
-          <nav>
-            <ul>
-              <li>
-                <a href="">
-                  Student Clearance System
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  About Us
-                </a>
-              </li>
-            </ul>
-          </nav>
-          {{-- <div class="copyright" id="copyright">
-            &copy; <script>
-              document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-            </script>, Designed by <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
-          </div> --}}
+        <!-- Footer Section-->
+        <footer class="footer">
+            <div class="container-fluid">
+            <nav>
+                <ul>
+                <li>
+                    <a href="">
+                    Student Clearance System
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                    About Us
+                    </a>
+                </li>
+                </ul>
+            </nav>
+            {{-- <div class="copyright" id="copyright">
+                &copy; <script>
+                document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+                </script>, Designed by <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
+            </div> --}}
+            </div>
+        </footer>
         </div>
-      </footer>
     </div>
-  </div>
   <!--   Core JS Files   -->
   <script src="../assets/js/core/jquery.min.js"></script>
   <script src="../assets/js/core/popper.min.js"></script>
