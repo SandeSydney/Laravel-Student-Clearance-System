5f440a254db2e4d78b271a1f6daf989a769da6e1
SandeSydney <sandesydney01@gmail.com>
2020-11-06 06:37:51 +0300

    Created a controller instance

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,16 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('role:administrator');
+    }
+
     public function index(){
         return view('admin.index');
     }
