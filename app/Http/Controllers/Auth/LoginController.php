48c40b82cf60717463d11a014f55d88fb1316389
SandeSydney <sandesydney01@gmail.com>
2020-11-06 06:37:00 +0300

    Redirect functionality added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,16 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // check role of user/admin to redirect them to their pages
+    protected function authenticated(Request $request, $user){
+        if ($user->hasRole('administrator')) {
+            return redirect('/admin');
+        }
+
+        if ($user->hasRole('user')) {
+            return redirect('/home');
+        }
+    }
 
     /**
      * Create a new controller instance.
