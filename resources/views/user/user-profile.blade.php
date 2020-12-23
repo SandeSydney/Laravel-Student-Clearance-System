106502d5c46ff4a76a164796960793ed12a67a8d
SandeSydney <sandesydney01@gmail.com>
2020-12-23 08:16:26 +0300

    Included session messages and image upload

--- resources/views/user/user-profile.blade.php
+++ resources/views/user/user-profile.blade.php
@@ -5,17 +5,35 @@
 @endsection
 
 @section('content')
+
+{{-- Show the session message using component created --}}
+<x-alert/>
+
 <div class="card border p-4 mt-1 mb-1">
     <div class="card-body row">
-        <div class="col-md-6 text-center pr-3">
-            {{-- defualt profile image --}}
-            <img src="{{asset('assets/img/default-avatar.png')}}" class="rounded-circle border img-fluid" alt="Profile Image">
-            {{-- Choose profile image --}}
-            <form action="/imageUpload" method="POST" class="p-3" enctype="multipart/form-data">
-                @csrf
-                <input type="file" name="image" class="p-1">
-                <input type="submit" value="Upload" class="p-1">
+        <div class="col-md-6 ">
+            {{-- Check if the profileImage is present, provide a default value --}}
+            @if(Auth::user()->profileImage)
+            {{-- profile image --}}
+            <img src="{{asset('/storage/images/'.Auth::user()->profileImage)}}" class="img-thumbnail border text-center m-auto" alt="Profile Image" width="70%" height="70%">
+            @else
+            {{-- Default profile picture --}}
+            <img src="{{asset('assets/img/default-avatar.png')}}" class="rounded-circle border text-center" alt="Profile Image">
             </form>
+            @endif
+            {{-- Choose profile image --}}
+            <form action="/imageUpload" method="POST" class="p-1" enctype="multipart/form-data">
+                @csrf
+                <div class="row">
+                    <div class="col-md-6">
+                        <input type="file" name="image" class="p-1 text-left">
+                    </div>
+                    <div class="col-md-6">
+                        <input type="submit" value="Upload" class="p-1 ml-1 text-left">
+                    </div>
+                </div>
+            </form>
+
         </div>
         <div class="col-md-6 p-2 card-body">
             <div class="row">
