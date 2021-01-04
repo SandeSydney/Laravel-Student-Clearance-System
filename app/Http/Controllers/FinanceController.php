910a1cb94387a4ce41ada5c7c917d880209f20a2
SandeSydney <sandesydney01@gmail.com>
2021-01-04 05:45:03 +0300

    Included routes in the module

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class FinanceController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('role:administrator');
+    }
+
+    public function index(){
+        return view('finance.index');
+    }
 }
