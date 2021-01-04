a92726ec35df3ebe3a7a5c810a1527fab7a4ca4f
SandeSydney <sandesydney01@gmail.com>
2021-01-04 05:58:42 +0300

    Extended the master layout

---
+++ resources/views/finance/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('title')
+    Finance | MSUSCI:SCS
+@endsection
+
+@section('navbar-brand')
+    Dashboard
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('scripts')
+
+@endsection
