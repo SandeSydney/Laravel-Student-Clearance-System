6c61fe32694f632b7daa20cf29bc3bccb387e7b1
SandeSydney <sandesydney01@gmail.com>
2021-01-05 07:20:05 +0300

    Added a library view

---
+++ resources/views/library/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('title')
+    Library | MSUSCI:SCS
+@endsection
+
+@section('navbar-brand')
+    Dashboard
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('scripts')
+
+@endsection
