184c2bfd49e369c686e1f0bef73376d5a06efc58
SandeSydney <sandesydney01@gmail.com>
2021-01-05 07:18:23 +0300

    New librarry controller and added middleware

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class LibraryController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('role:libraryadmin');
+    }
+
+    public function index(){
+        return view('library.index');
+    }
 }
