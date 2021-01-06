8283da85172ffe06cf34623feef3dab9af9cdd10
SandeSydney <sandesydney01@gmail.com>
2021-01-06 06:56:38 +0300

    Created an admin dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,35 @@
 @endsection
 
 @section('content')
-
+    <div class="container">
+        <div class="row m-1">
+            <div class="col-sm-3 border m-2">
+                <h4><u>System Users</u></h4>
+                <p>Population:</p>
+            </div>
+            <div class="col-sm-3 border m-2">
+                <h4><u>Finance Dept.</u></h4>
+                <p>Population:</p>
+            </div>
+            <div class="col-sm-3 border m-2">
+                <h4><u>Library Dept.</u></h4>
+                <p>Population:</p>
+            </div>
+        </div>
+        <div class="row m-1">
+            <div class="col-sm-3 border m-2">
+                <h4><u>Cleared students</u></h4>
+                <p>Population:</p>
+            </div>
+            <div class="col-sm-3 border m-2">
+                <h4><u>Pending Clearance</u></h4>
+                <p>Population:</p>
+            </div>
+            <div class="col-sm-3 border m-2">
+                <h4><u>Manage Users</u></h4>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
