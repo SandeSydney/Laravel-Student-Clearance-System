ec0f22f2965176f6ce89b2cfb0055714ec46b296
SandeSydney <sandesydney01@gmail.com>
2021-01-07 09:04:35 +0300

    Made aesthetic changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,30 +11,26 @@
 @section('content')
     <div class="container">
         <div class="row m-1">
-            <div class="col-sm-3 border m-2">
+            <div class="col-sm-3 border rounded m-2" style="background-color: #2CA8FF">
                 <h4><u>System Users</u></h4>
                 <p>Population:</p>
             </div>
-            <div class="col-sm-3 border m-2">
-                <h4><u>Finance Dept.</u></h4>
+            <div class="col-sm-3 border rounded m-2" style="background-color: #2CA8FF">
+                <h4><u>Finance Section</u></h4>
                 <p>Population:</p>
             </div>
-            <div class="col-sm-3 border m-2">
-                <h4><u>Library Dept.</u></h4>
+            <div class="col-sm-3 border m-2" style="background-color: #2CA8FF">
+                <h4><u>Library Section</u></h4>
                 <p>Population:</p>
             </div>
         </div>
         <div class="row m-1">
-            <div class="col-sm-3 border m-2">
-                <h4><u>Cleared students</u></h4>
-                <p>Population:</p>
+            <div class="col-sm-3">
             </div>
-            <div class="col-sm-3 border m-2">
-                <h4><u>Pending Clearance</u></h4>
-                <p>Population:</p>
-            </div>
-            <div class="col-sm-3 border m-2">
+            <div class="col-sm-3 border ml-4 mt-2 mb-2" >
                 <h4><u>Manage Users</u></h4>
+            </div>
+            <div class="col-sm-3">
             </div>
         </div>
     </div>
