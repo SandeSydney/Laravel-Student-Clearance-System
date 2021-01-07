e1a2eb3feeeea45a6980ec015949ea8bdbb57607
SandeSydney <sandesydney01@gmail.com>
2021-01-07 09:05:02 +0300

    Added default Laravel error handlers

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,12 +1,22 @@
 <div>
     @if(session()->has('message'))
-    <div class="alert alert-success ml-5 mr-5 mt-2 text-center">
-        {{session()->get('message')}}
-    </div>
+        <div class="alert alert-success ml-5 mr-5 mt-2 text-center">
+            {{session()->get('message')}}
+        </div>
     @elseif(session()->has('error'))
-    <div class="alert alert-danger ml-5 mr-5 mt-2 text-center">
-        {{session()->get('error')}}
-    </div>
+        <div class="alert alert-danger ml-5 mr-5 mt-2 text-center">
+            {{session()->get('error')}}
+        </div>
+    @endif
+
+    @if ($errors->any())
+        <div class="alert alert-danger ml-5 mr-5 mt-2 text-center">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
     @endif
 
 </div>
