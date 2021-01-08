938772eb39e3bb4f643bcd9abb6f3e0283790c38
SandeSydney <sandesydney01@gmail.com>
2021-01-08 07:21:28 +0300

    Added ability to close off the alerts

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,16 +1,19 @@
 <div>
     @if(session()->has('message'))
         <div class="alert alert-success ml-5 mr-5 mt-2 text-center">
+            <a href="#" class="close text-right" data-dismiss="alert" aria-label="close">&times;</a>
             {{session()->get('message')}}
         </div>
     @elseif(session()->has('error'))
         <div class="alert alert-danger ml-5 mr-5 mt-2 text-center">
+            <a href="#" class="close text-right" data-dismiss="alert" aria-label="close">&times;</a>
             {{session()->get('error')}}
         </div>
     @endif
 
     @if ($errors->any())
         <div class="alert alert-danger ml-5 mr-5 mt-2 text-center">
+            <a href="#" class="close text-right" data-dismiss="alert" aria-label="close">&times;</a>
             <ul>
                 @foreach($errors->all() as $error)
                     <li>{{ $error }}</li>
