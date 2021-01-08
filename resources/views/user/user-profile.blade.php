70cdddc0e98b4511979e585ec0d0fdef98407c5b
SandeSydney <sandesydney01@gmail.com>
2021-01-08 06:18:37 +0300

    Getting student data from the database

--- resources/views/user/user-profile.blade.php
+++ resources/views/user/user-profile.blade.php
@@ -43,20 +43,20 @@
         </div>
         <div class="col-md-6 p-2 card-body">
             <div class="row">
-                <div class="col-md-2 font-weight-bold">Name:</div>
-                <div class="col-md-10">Simiyu Sydney</div>
-                <div class="col-md-2 font-weight-bold">Adm:</div>
-                <div class="col-md-10">CI/00121/016</div>
-                <div class="col-md-2 font-weight-bold">Email:</div>
-                <div class="col-md-10">sandesydney02@gmail.com</div>
-                <div class="col-md-2 font-weight-bold">Phone No.:</div>
-                <div class="col-md-10">0728124585</div>
-                <div class="col-md-2 font-weight-bold">ID No.:</div>
-                <div class="col-md-10">34641101</div>
-                <div class="col-md-2 font-weight-bold">Course:</div>
-                <div class="col-md-10">Bachelor of Science in Information Technology</div>
-                <div class="col-md-2 font-weight-bold">Gender:</div>
-                <div class="col-md-10">Male</div>
+                <div class="col-md-4 font-weight-bold">Name:</div>
+                <div class="col-md-8">{{Auth::user()->name}}</div>
+                <div class="col-md-4 font-weight-bold">Reg Number:</div>
+                <div class="col-md-8">{{Auth::user()->regNumber}}</div>
+                <div class="col-md-4 font-weight-bold">Email:</div>
+                <div class="col-md-8">{{Auth::user()->email}}</div>
+                <div class="col-md-4 font-weight-bold">Phone No.:</div>
+                <div class="col-md-8">{{Auth::user()->phoneNumber}}</div>
+                <div class="col-md-4 font-weight-bold">ID No.:</div>
+                <div class="col-md-8">{{Auth::user()->IdNumber}}</div>
+                <div class="col-md-4 font-weight-bold">Course:</div>
+                <div class="col-md-8">{{Auth::user()->course}}</div>
+                <div class="col-md-4 font-weight-bold">Gender:</div>
+                <div class="col-md-8">{{Auth::user()->gender}}</div>
             </div>
         </div>
     </div>
