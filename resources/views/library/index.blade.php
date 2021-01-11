4e85b328f879cf49c9323303481e2c017db69074
SandeSydney <sandesydney01@gmail.com>
2021-01-11 06:49:10 +0300

    modified the library form and added a view of the statuses

--- resources/views/library/index.blade.php
+++ resources/views/library/index.blade.php
@@ -14,21 +14,22 @@
             <h2>Library Section</h2>
         </div>
         <hr>
+        <x-alert />
         <div class="border">
-            <form class="form-horizontal p-2" action="" style="background-color: #fdfdfd" role="form">
+            <form class="form-horizontal p-2" method="POST" action="/library/store" style="background-color: #fdfdfd" role="form">
                 @csrf
                 <div class="form-group">
                     <label for="regNUmber" class="control-label col-sm-2">Admission Number</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="regNumber" placeholder="CI/00001/020" required>
+                        <input type="text" class="form-control" id="regNumber" name="regNumber" placeholder="CI/00001/020" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="accBalance" class="control-label col-sm-2">Cleared</label>
+                    <label for="libStatus" class="control-label col-sm-2">Library Status</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="accBalance" placeholder="Key in 1 for Yes, 0 for No" required>
+                        <input id="libStatus" type="radio" name="libStatus" value="Cleared" required> Cleared <br>
+                        <input id="libStatus" type="radio" name="libStatus" value="Not Cleared" checked required> Not Cleared <br>
                     </div>
-
                 </div>
                 <div class="form-group">
                     <div class="col-sm-offset-4 col-sm-8 text-center">
@@ -37,6 +38,22 @@
                     </div>
                 </div>
             </form>
+            <hr>
+            <div>
+                <h4>Student Library Statuses</h4>
+                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#libraries">Click to View</button>
+                <div id="libraries" class="collapse flex">
+                    <ol>
+                    @foreach ($libraries as $library)
+                        <li>
+                            {{ $library->regNumber}} : {{ $library->libStatus }} &nbsp;
+                            <a href="{{'/library/ '.$library->id.'/edit'}}" class="btn btn-warning">Edit</a>
+                        </li>
+                    @endforeach
+                </ol>
+                </div>
+
+            </div>
         </div>
     </div>
 @endsection
