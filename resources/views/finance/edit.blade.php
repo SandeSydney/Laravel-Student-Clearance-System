fb4de564a25c516f953acab4f167cbf037c06b95
SandeSydney <sandesydney01@gmail.com>
2021-01-12 06:09:30 +0300

    Made aesthetic changes

--- resources/views/finance/edit.blade.php
+++ resources/views/finance/edit.blade.php
@@ -2,20 +2,24 @@
 
 @section('content')
 <div class="container">
+    <div>
+        <a class="btn btn-warning " href="/finance">Back</a>
+    </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header text-center">{{ __('Edit Finances') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="">
+                    <form method="POST" action="{{ route('finance.update', $finance->id) }}">
                         @csrf
+                        @method('patch')
 
                         <div class="form-group row">
                             <label for="regNumber" class="col-md-4 col-form-label text-md-right">{{ __('Admission Number') }}</label>
 
                             <div class="col-md-6">
-                                <input id="regNumber" type="text" class="form-control @error('regNumber') is-invalid @enderror" name="regNumber" value="{{ old('regNumber') }}" required>
+                                <input id="regNumber" type="text" class="form-control @error('regNumber') is-invalid @enderror" name="regNumber" value="{{ $finance->regNumber }}" required>
 
                                 @error('regNumber')
                                     <span class="invalid-feedback" role="alert">
@@ -28,7 +32,7 @@
                             <label for="accBalance" class="col-md-4 col-form-label text-md-right">{{ __('Account Balance') }}</label>
 
                             <div class="col-md-6">
-                                <input id="accBalance" type="text" class="form-control @error('accBalance') is-invalid @enderror" name="accBalance" value="{{ old('accBalance') }}" required>
+                                <input id="accBalance" type="text" class="form-control @error('accBalance') is-invalid @enderror" name="accBalance" value="{{ $finance->accBalance }}" required>
 
                                 @error('accBalance')
                                     <span class="invalid-feedback" role="alert">
