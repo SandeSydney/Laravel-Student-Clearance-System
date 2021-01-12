9add31e9687d3bf65136c3b5baa68096e0314629
SandeSydney <sandesydney01@gmail.com>
2021-01-12 21:00:47 +0300

    Incorporated Database Models

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
+use App\Models\Finance;
+
+use App\Models\Library;
+
 class AdminController extends Controller
 {
     /**
@@ -16,16 +22,14 @@ class AdminController extends Controller
         $this->middleware('role:superadministrator');
     }
 
-    public function index(){
-        return view('admin.index');
-    }
-    public function notifications(){
-        return view('admin.notifications');
-    }
-    public function user(){
-        return view('admin.user');
-    }
-    public function tables(){
-        return view('admin.tables');
+    public function index(Request $request){
+
+        $users = User::all();
+
+        $finances = Finance::all();
+
+        $libraries = Library::all();
+
+        return view('admin.index')->with(['finances'=>$finances, 'users'=>$users, 'libraries' => $libraries]);
     }
 }
